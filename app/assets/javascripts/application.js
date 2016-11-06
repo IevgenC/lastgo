cdd64b30de292b441dc4f2435d16a1b0bfec4ed5
IevgenC <cherya.chernikov@gmail.com>
2016-11-06 22:51:50 +0000

    thrall updated with data from json

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,13 +15,34 @@
 //= require turbolinks
 //= require_tree .
 
-var data = {
-    0: "Desc 0",
-    1: "Desc 1",
-    2: "Desc 2",
-    3: "Desc 3"
-};
+//var data = {
+//    0: "Desc 0",
+//    1: "Desc 1",
+//    2: "Desc 2",
+//    3: "Desc 3"
+//};
 
-function showDiv(data_id, desc_id) {
-    document.getElementById(desc_id).innerHTML = data[data_id];
+//function showDiv(data_id, desc_id) {
+//    document.getElementById(desc_id).innerHTML = data[data_id];
+//}
+
+var page_content;
+$.get( "/assets/thrall.json", function(data){
+    page_content = data;
+});
+
+function getImage(id) {
+	document.getElementById(id).src = page_content['talents'][id]['talent_image'];
 }
+
+function showText(name_id, text_id, element_id){
+	document.getElementById(name_id).innerHTML = page_content['talents'][element_id]['talent_name'];
+	document.getElementById(text_id).innerHTML = page_content['talents'][element_id]['talent_desc'];
+}
+
+$(document).ready(function() {
+$('img').click(function(){
+    $(this).siblings('img').removeClass('selected');
+    $(this).addClass('selected');
+});
+});
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,18 @@
+require 'json'
+
 module PagesHelper
+	def read_json
+		file = File.read('app/assets/jsons/thrall.json')
+		JSON.parse(file)
+	end	
+	def hero_name
+		data = read_json
+		data['hero_name']
+	end
+
+	def talent_image(id)
+		data = read_json
+		data['talents'][id]['talent_image']
+	end
+
 end
